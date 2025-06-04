b4d3d7240f8418f52b20fe24dd510f3ec1f1c844
Clément Muth <clementmuth@gmail.com>
2025-06-04 17:39:14 +0200

    fix(shopping-lists): update import statement for ShoppingListDetails component

--- apps/pcomparator/src/app/[locale]/shopping-lists/[id]/page.tsx
+++ apps/pcomparator/src/app/[locale]/shopping-lists/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { auth } from "~/libraries/nextauth/authConfig";
-import { getShoppingList } from "~/packages/applications/shopping-lists/src";
+import { ShoppingListDetails, getShoppingList } from "~/packages/applications/shopping-lists/src";
 
 export default async function ShoppingListPage({ params }: { params: Promise<{ id: string }> }) {
   const shoppingListId = (await params).id;
@@ -13,7 +13,6 @@ export default async function ShoppingListPage({ params }: { params: Promise<{ i
     <main className="flex w-full justify-center p-4">
       <div className="flex flex-col gap-y-8 max-w-4xl w-full">
         <div className="max-w-3xl mx-auto w-full pb-8">
-          {/* @ts-ignore */}
           <ShoppingListDetails list={list.toObject()} user={session?.user} />
         </div>
       </div>
